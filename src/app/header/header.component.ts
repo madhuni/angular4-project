599b63413f8f9c9687d50eb7efec6e54963fbfa2
Kanishka Mohan Madhuni <kanishkamohanmadhuni@gmail.com>
2017-11-05 18:54:43 +0530

    Added navigation bar for the website
    Mobile/Desktop designs are prepared.
    CSS is included for the navigation.

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  public isIn = false;
+
+  toggleState() {
+    let bool: boolean = this.isIn;
+    this.isIn = bool === false ? true : false;
+    console.log('the condition is : ', this.isIn);
+  }
+
+  ngOnInit() {}
+}
