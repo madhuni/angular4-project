b1c157fe2ae3f55a75f2e2487f7fb654e1c78f2d
Kanishka Mohan Madhuni <kanishkamohanmadhuni@gmail.com>
2017-11-06 01:30:05 +0530

    Added contact-us page for the website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule
