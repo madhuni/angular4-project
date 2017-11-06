7af4b4ba5904e0f373d8abc2248632a38246afb9
Kanishka Mohan Madhuni <kanishkamohanmadhuni@gmail.com>
2017-11-06 20:11:20 +0530

    Routing for the application is configured

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -10,7 +11,16 @@ import { ProductsComponent } from './products/products.component';
 import { EtherealHaloComponent } from './products/ethereal-halo/ethereal-halo.component';
 import { EtherealPentagramComponent } from './products/ethereal-pentagram/ethereal-pentagram.component';
 import { EtherealEstrellaComponent } from './products/ethereal-estrella/ethereal-estrella.component';
+import { HomeComponent } from './home/home.component';
 
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'products/halo', component: EtherealHaloComponent },
+  { path: 'products/pentagram', component: EtherealPentagramComponent },
+  { path: 'products/estrella', component: EtherealEstrellaComponent },
+  { path: 'contact-us', component: ContactUsComponent },
+  { path: 'company', component: CompanyComponent }
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,10 +31,12 @@ import { EtherealEstrellaComponent } from './products/ethereal-estrella/ethereal
     ProductsComponent,
     EtherealHaloComponent,
     EtherealPentagramComponent,
-    EtherealEstrellaComponent
+    EtherealEstrellaComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
