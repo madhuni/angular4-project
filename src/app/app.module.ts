8424576b7c066660775312e141fa8250f389fefe
Kanishka Mohan Madhuni <kanishkamohanmadhuni@gmail.com>
2017-11-06 19:19:14 +0530

    Added products page for the website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,10 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { CompanyComponent } from './company/company.component';
+import { ProductsComponent } from './products/products.component';
+import { EtherealHaloComponent } from './products/ethereal-halo/ethereal-halo.component';
+import { EtherealPentagramComponent } from './products/ethereal-pentagram/ethereal-pentagram.component';
+import { EtherealEstrellaComponent } from './products/ethereal-estrella/ethereal-estrella.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +17,11 @@ import { CompanyComponent } from './company/company.component';
     HeaderComponent,
     FooterComponent,
     ContactUsComponent,
-    CompanyComponent
+    CompanyComponent,
+    ProductsComponent,
+    EtherealHaloComponent,
+    EtherealPentagramComponent,
+    EtherealEstrellaComponent
   ],
   imports: [
     BrowserModule
