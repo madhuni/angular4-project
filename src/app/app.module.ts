87a40baa595dd345ca6582244ed5b150d617c4cb
Kanishka Mohan Madhuni <kanishkamohanmadhuni@gmail.com>
2017-11-06 13:41:28 +0530

    Added 'about-company' page for the website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
+import { CompanyComponent } from './company/company.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    ContactUsComponent
+    ContactUsComponent,
+    CompanyComponent
   ],
   imports: [
     BrowserModule
