ddb5a4ce20274e1a3b8c274111142d73821eb963
Kanishka Mohan Madhuni <kanishkamohanmadhuni@gmail.com>
2017-11-07 16:12:16 +0530

    feature: Added routing behaviour to each link of contact-us section in footer part

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
@@ -6,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  loadGeneralQuery() {
+    this.router.navigate(['contact-us/general-queries']);
+  }
+
+  loadCareers() {
+    this.router.navigate(['contact-us/careers']);
+  }
+
+  loadMediaQuery() {
+    this.router.navigate(['contact-us/media-enquiries']);
+  }
+
+  loadBecomeReseller() {
+    this.router.navigate(['contact-us/become-a-reseller-distributor']);
+  }
 }
