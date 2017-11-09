de4c0769a1e1a5ec49cb603501e47a12a1b892e5
Kanishka Mohan Madhuni <kanishkamohanmadhuni@gmail.com>
2017-11-09 13:28:07 +0530

    Added media page for the website

--- src/app/media/media.component.ts
+++ src/app/media/media.component.ts
@@ -8,6 +8,26 @@ export class MediaComponent implements OnInit {
 
   constructor() { }
 
+  public contents: any[] = [{
+    imgLink: 'http://www.emwcenter.com/wp-content/uploads/Online-articles.jpg',
+    mediaLink: '/media',
+    heading: 'Sample Text for the head line text',
+    date: 'May 2014',
+    publisher: 'YourStory'
+  }, {
+    imgLink: 'https://s3.amazonaws.com/marquee-test-akiaisur2rgicbmpehea/NgZHitNrQkqNOedVqpbl_EditorNote.jpg',
+    mediaLink: '/media',
+    heading: 'Sample Text for the head line text',
+    date: 'May 2014',
+    publisher: 'YourStory'
+  }, {
+    imgLink: 'http://www.radio.gov.pk/digital_images/large/2017-01-20/top-news-stories-and-editorials-from-national-newspapers-1484914847-3271.jpg',
+    mediaLink: '/media',
+    heading: 'Sample Text for the head line text',
+    date: 'May 2014',
+    publisher: 'YourStory'
+  }];
+
   ngOnInit() {
   }
 
