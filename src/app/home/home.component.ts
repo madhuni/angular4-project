f99e1f9d6a4e1ff44cf6b63cc3ec091620b59b4a
Kanishka Mohan Madhuni <kanishkamohanmadhuni@gmail.com>
2017-11-10 09:56:29 +0530

    New build

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import $ from 'jquery';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html'
@@ -10,6 +10,8 @@ export class HomeComponent implements OnInit {
   constructor(private router: Router) { }
 
   ngOnInit() {
+    const container = $('body');
+    container.removeClass('padding-on').addClass('padding-off');
   }
 
   loadHaloPage() {
